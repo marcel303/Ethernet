6c253364083372d73614f5d8a51d1d4c9c2286d0
Marcel Smit <marcel303@gmail.com>
2020-10-14 11:33:25 +0200

    Add begin method which accepts a port number.

--- src/EthernetServer.cpp
+++ src/EthernetServer.cpp
@@ -25,8 +25,10 @@
 uint16_t EthernetServer::server_port[MAX_SOCK_NUM];
 
 
-void EthernetServer::begin()
+void EthernetServer::begin(uint16_t port)
 {
+	if (port)
+		_port = port;
 	uint8_t sockindex = Ethernet.socketBegin(SnMR::TCP, _port);
 	if (sockindex < MAX_SOCK_NUM) {
 		if (Ethernet.socketListen(sockindex)) {
